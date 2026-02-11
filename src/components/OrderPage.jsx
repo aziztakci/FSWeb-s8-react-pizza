8c55d8e14c846087fc4aeeee8e33017f53be1e28
aziztakci <takciazizz@gmail.com>
2026-02-11 15:55:24 +0300

    feat: implement OrderPage header with styled-components

--- src/components/OrderPage.jsx
+++ src/components/OrderPage.jsx
@@ -1,12 +1,39 @@
-export default function OrderPage (props) {
+import styled from "styled-components";
 
-    const {setActivePage} = props;
-    return (
-        <div className="orderHeader">
-            <img src="/assets/iteration-1/logo.svg" alt="" />
-            <nav>
-                
-            </nav>
-        </div>
-    )
+const ButtonHome = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  color: #000000;
+  cursor: pointer;
+  font-family: 'Barlow', sans-serif;;
+  font-size: 16px;
+
+  &:hover {
+  background-color: #f38383;
+  border-radius: 10px;
+    text-decoration: underline;
+  }
+`
+
+export default function OrderPage(props) {
+  const { setActivePage } = props;
+  return (
+    <div className="order-page-container">
+      <header className="order-header">
+         <img src="/assets/iteration-1/logo.svg" alt="teknolojik-yemekler" />
+         <div>
+           <nav className="page-buttons">
+             <ButtonHome className="page-btn" onClick={() => setActivePage("home")}>
+               Anasayfa
+             </ButtonHome>
+             <span className="page-seperator">-</span>
+             <span className="page-active">Sipariş Oluştur</span>
+           </nav>
+         </div>
+      </header>
+
+
+    </div>
+  );
 }
